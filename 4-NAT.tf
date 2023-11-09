2a62ce4945b2bdee2496302affed86496e948e40
aescott23 <145212089+aescott23@users.noreply.github.com>
2023-11-08 20:34:30 -0600

    Update 4-NAT.tf

--- 4-NAT.tf
+++ 4-NAT.tf
@@ -8,11 +8,14 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public-eu-west-1a.id
+  subnet_id     = aws_subnet.public-ap-southeast-2a.id
 
-  tags = {
-    Name = "nat"
+   tags = {
+    Name = "app1"
+    Service = "application1"
+    Owner = "Chewbacca"
+    Planet = "Mustafar"
   }
 
-  depends_on = [aws_internet_gateway.igw]
+  depends_on = [aws_internet_gateway.app1_igw]
 }
