c84223906c12a7a19a1f41667c4d5d245d08e5a1
aescott23 <145212089+aescott23@users.noreply.github.com>
2023-11-08 20:33:47 -0600

    Update 3-IGW.tf

--- 3-IGW.tf
+++ 3-IGW.tf
@@ -1,10 +1,11 @@
-resource "aws_internet_gateway" "igw" {
+resource "aws_internet_gateway" "app1_igw" {
   vpc_id = aws_vpc.app1.id
 
   tags = {
-    Name    = "app1_IG"
+    Name = "app1_igw"
     Service = "application1"
-    Owner   = "Luke"
-    Planet  = "Musafar"
+    Owner = "Chewbacca"
+    Planet = "Mustafar"
+    
   }
 }
